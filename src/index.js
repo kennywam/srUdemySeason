ddc8ff91171d11bae7a2ad6e9dbfd79622660cef
Kennedy Wambua <kennywambu254@gmail.com>
2022-08-16 17:49:15 +0300

    updating functional and classical components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client"
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+import "./style/style/App.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+class App extends React.Component{
+    state = {pos: null, errorMessage: ''}
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) =>this.setState({lat: position.coords.latitude}),
+            (err) =>this.setState({errorMessage: err.message})
+        );
+    }
+    renderContent(){
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        if (!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+        return <Spinner message="Please wait while we access your location" /> 
+    }
+
+    
+    render(){
+        return(
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
+        }
+ }
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+)
